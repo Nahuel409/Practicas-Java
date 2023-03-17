38b3cc496efe96f2c918e5ced37d64cf5aefb325
Diego Villan <dnvx81@outlook.com>
2023-03-17 20:08:17 -0300

    pruebas del metodo main para certificacion oracle

---
+++ src/cuestionarios/pruebas.java
@@ -0,0 +1,24 @@
+package cuestionarios;
+
+/** @autor
+Diego NahueL Villan
+ **/
+public class pruebas {
+
+
+    /**@return
+     * esta es una funcion de prueba
+     *
+     */
+    public static void main(String[] args) {
+
+        char c = 'A';
+        c += 5;
+
+        System.out.println("el valor de c es "+ c);
+
+    }
+
+
+
+}
